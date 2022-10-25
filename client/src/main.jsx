087689b56a4df8aa67b92912435f100027b8b233
rohit <rhtry.tech@gmail.com>
2022-10-26 04:05:45 +0530

    wrapping with context-provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+// importing Context
+// ContextProvider > TransactionProvider
+import { TransactionProvider } from "./context/TransactionContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // Wrapping up the entire React project with the ContextProvider to access Blockchain data everywhere !
+  <TransactionProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </TransactionProvider>
+);
